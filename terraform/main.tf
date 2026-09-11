9d7b808430f153b6949fb1135745751e4e4afcc6
Okkema Labs <correos@okkema.org>
2026-09-10 22:57:17 -0400

    chore: update terraform modules and add deploy workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ locals {
 
 module "zone" {
   source  = "app.terraform.io/okkema/zone/cloudflare"
-  version = "~> 0.1"
+  version = "~> 1.0"
 
   zone_id = var.cloudflare_zone_id
 }
@@ -26,7 +26,7 @@ module "secrets" {
 
 module "database" {
   source  = "app.terraform.io/okkema/database/cloudflare"
-  version = "~> 0.1"
+  version = "~> 1.0"
 
   account_id = var.cloudflare_account_id
   name       = var.github_repository
@@ -34,24 +34,24 @@ module "database" {
 
 module "worker" {
   source  = "app.terraform.io/okkema/worker/cloudflare"
-  version = "~> 0.12"
+  version = "~> 1.0"
 
   account_id          = var.cloudflare_account_id
   zone_id             = var.cloudflare_zone_id
   name                = var.github_repository
   hostnames           = [var.github_repository]
-  content             = file(abspath("${path.module}/../dist/index.js"))
+  content_file        = abspath("${path.module}/../dist/index.js")
   compatibility_flags = toset(["nodejs_compat_v2"])
   secrets = [
-    { name = "SENTRY_DSN", value = module.sentry.dsn },
-    { name = "TMDB_TOKEN", value = var.TMDB_TOKEN },
+    { name = "SENTRY_DSN", text = module.sentry.dsn },
+    { name = "TMDB_TOKEN", text = var.TMDB_TOKEN },
   ]
   env_vars = [
-    { name = "OAUTH_AUDIENCE", value = local.audience },
-    { name = "OAUTH_TENANT", value = var.OAUTH_TENANT },
+    { name = "OAUTH_AUDIENCE", text = local.audience },
+    { name = "OAUTH_TENANT", text = var.OAUTH_TENANT },
   ]
   databases = [
-    { binding = "DB", id = module.database.id }
+    { name = "DB", id = module.database.id }
   ]
 
   depends_on = [module.database, module.sentry]
@@ -59,7 +59,7 @@ module "worker" {
 
 module "sentry" {
   source  = "app.terraform.io/okkema/project/sentry"
-  version = "~> 0.4"
+  version = "~> 0.7"
 
   github_organization = var.github_owner
   github_repository   = var.github_repository
@@ -67,11 +67,14 @@ module "sentry" {
 
 module "server" {
   source  = "app.terraform.io/okkema/server/auth0"
-  version = "~> 0.1"
+  version = "~> 1.0"
 
   name       = var.github_repository
   identifier = local.audience
-  scopes = {
-    "read:movies" : "Read TMDB movies",
-  }
+  scopes = [
+    {
+      name        = "read:movies"
+      description = "Read TMDB movies"
+    },
+  ]
 }
